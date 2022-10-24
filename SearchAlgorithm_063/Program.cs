90588cbd707859fb16714688d85cdaefc99de6c7
S Chandra <s.chandra.ft21@mail.umy.ac.id>
2022-10-24 16:28:03 +0700

    mengulang

--- SearchAlgorithm_063/Program.cs
+++ SearchAlgorithm_063/Program.cs
@@ -15,16 +15,7 @@ namespace SearchAlgorithm_063
         //Get the number of elements to store in the array
         int i;
 
-        public void input()
-        {
-            Console.Write("Enter the number of elements in the array: ");
-            string s = Console.ReadLine();
-            n = Int32.Parse(s);
-            if ((n > 0) & (n <= 20))
-                break;
-            else
-                Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements.\n");
-        }
+
         static void Main(string[] args)
         {
         }
